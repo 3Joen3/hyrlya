3315e48a7d675fcdd914967f2b28991d74c0e045
3Joen3 <joalevall@gmail.com>
2025-09-09 16:37:19 +0200

    Added a redirect from the auth page to /landlord

--- web/app/(public)/auth/page.tsx
+++ web/app/(public)/auth/page.tsx
@@ -1,11 +1,17 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import Page from "../../components/Page";
 import AuthForm from "../../components/Forms/AuthForm";
 
-export default function page() {
+export default async function page() {
+  const cookieStore = await cookies();
+  if (cookieStore.get("__Host-accessToken")?.value) {
+    redirect("/landlord");
+  }
+
   return (
     <Page>
-      <AuthForm className="w-full lg:w-1/3 justify-self-center mt-10"/>
+      <AuthForm className="w-full lg:w-1/3 justify-self-center mt-10" />
     </Page>
   );
 }
-  
