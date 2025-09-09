e1a9ce81a3e32cf6222156868724622e34caa2c7
3Joen3 <joalevall@gmail.com>
2025-09-10 01:16:10 +0200

    Added a page for listings

--- web/app/components/Headers/LandlordHeader.tsx
+++ web/app/components/Headers/LandlordHeader.tsx
@@ -1,9 +1,23 @@
 import Header from "./Header";
+import NavLink from "../NavLink";
 
 export default function LandlordHeader() {
+  const headerLinks = [{ href: "landlord/listings", title: "Annonser" }];
+
   return (
-    <Header>
-      <p>LandlordHeader</p>
+    <Header className="gap-14" logoHref="/landlord">
+      <div className="space-x-6">
+        {headerLinks.map((link, index) => (
+          <NavLink
+            key={`header-link-${index}`}
+            href={link.href}
+            color="primary"
+            className="px-4"
+          >
+            {link.title}
+          </NavLink>
+        ))}
+      </div>
     </Header>
   );
 }
