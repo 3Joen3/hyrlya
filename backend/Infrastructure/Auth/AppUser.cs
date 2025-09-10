aedd01121039a9034bbd2b4944cd3d45e36be08f
3Joen3 <joalevall@gmail.com>
2025-09-10 04:42:10 +0200

    Changed the name of the identity User to AppUser

---
+++ backend/Infrastructure/Auth/AppUser.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HyrLya.Infrastructure.Auth
+{
+    public class AppUser : IdentityUser { }
+}
--- backend/Infrastructure/User.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace HyrLya.Infrastructure
-{
-    public class User : IdentityUser { }
-}
