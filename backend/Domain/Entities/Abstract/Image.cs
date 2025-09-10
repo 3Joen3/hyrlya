f375c7b6ac26abd511ca829321a1ab67121bc4ef
3Joen3 <joalevall@gmail.com>
2025-09-11 00:26:05 +0200

    Added an abstract class for Image

---
+++ backend/Domain/Entities/Abstract/Image.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities.Abstract
+{
+    public abstract class Image : Entity
+    {
+        public string Url { get; private set; }
+        public string AltText { get; private set; }
+
+        protected Image(string url, string altText)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(url);
+            ArgumentException.ThrowIfNullOrWhiteSpace(altText);
+
+            Url = url;
+            AltText = altText;
+        }
+
+        protected Image() { Url = default!; AltText = default!; }
+    }
+}
