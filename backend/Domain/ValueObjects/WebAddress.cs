c422eeda11e7b7ff22c62a2bed8cc75dde481ab7
3Joen3 <joalevall@gmail.com>
2025-09-11 14:13:18 +0200

    WebAddress ValueObject

---
+++ backend/Domain/ValueObjects/WebAddress.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.ValueObjects
+{
+    public record WebAddress
+    {
+        public string Value { get; }
+
+        public WebAddress(string url)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(url);
+
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                throw new ArgumentException("Invalid web address.", nameof(url));
+
+            Value = url;
+        }
+
+        private WebAddress() { Value = default! }
+    }
+}
---
+++ backend/Tests/Unit/ValueObjects/WebAddressTests.cs
@@ -0,0 +1,40 @@
+﻿using Domain.ValueObjects;
+
+namespace Tests.Unit.ValueObjects
+{
+    public class WebAddressTests
+    {
+        [Theory]
+        [InlineData("https://example.com")]
+        [InlineData("https://www.example.com")]
+        [InlineData("http://localhost:5000")]
+        public void Constructor_WithValidUrl_ShouldSucceed(string url)
+        {
+            var webAddress = new WebAddress(url);
+
+            Assert.Equal(url, webAddress.Value);
+        }
+
+        [Fact]
+        public void Constructor_WithNullUrl_ShouldThrow()
+        {
+            string? url = null;
+
+            Assert.Throws<ArgumentNullException>(()
+                => new WebAddress(url!));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("SomeWords")]
+        [InlineData("example.com")]
+        [InlineData("htps://example.com")]
+        [InlineData("https://SomeWords")]
+        public void Constructor_WithInvalidUrl_ShouldThrow(string url)
+        {
+            Assert.Throws<ArgumentException>(()
+                => new WebAddress(url));
+        }
+    }
+}
