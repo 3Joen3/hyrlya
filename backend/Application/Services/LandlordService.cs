6fefda339a26049d4149693e5b7c792195a164d6
3Joen3 <joalevall@gmail.com>
2025-09-11 21:32:54 +0200

    Add Landlord Service

---
+++ backend/Application/Services/LandlordService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services
+{
+    public class LandlordService : ILandlordService
+    {
+        public Task<Landlord> GetByIdentityId(string identityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
