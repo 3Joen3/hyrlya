12754ec48de49add2d67fb68903b2da0434b6345
3Joen3 <joalevall@gmail.com>
2025-09-11 02:45:31 +0200

    Had to change the test

--- backend/Tests/Unit/Entities/LandlordTests.cs
+++ backend/Tests/Unit/Entities/LandlordTests.cs
@@ -13,10 +13,19 @@ namespace Tests.Unit.Entities
 
             Assert.NotEqual(landlord.Id, Guid.Empty);
             Assert.Equal(identityId, landlord.IdentityId);
+            Assert.Null(landlord.Profile);
+        }
+
+        [Fact]
+        public void Constructor_WithNullIdentityId_ShouldThrow()
+        {
+            string? identityId = null;
+
+            Assert.Throws<ArgumentNullException>(()
+                =>  new Landlord(identityId!));
         }
 
         [Theory]
-        [InlineData(null)]
         [InlineData("")]
         [InlineData("  ")]
         public void Constructor_WithInvalidIdentityId_ShouldThrow(string identityId)
