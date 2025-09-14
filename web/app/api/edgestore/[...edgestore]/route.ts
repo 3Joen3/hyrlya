a93d8a7839698ef39098e014eb8f79c84bec529c
3Joen3 <joalevall@gmail.com>
2025-09-14 23:21:14 +0200

    Add some endpoints or whatever it's called when uploading images

--- web/app/api/edgestore/[...edgestore]/route.ts
+++ web/app/api/edgestore/[...edgestore]/route.ts
@@ -1,10 +1,18 @@
 import { initEdgeStore } from "@edgestore/server";
+import { z } from "zod";
 import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
 
 const es = initEdgeStore.create();
 
 const edgeStoreRouter = es.router({
-  publicImages: es.imageBucket(),
+  publicImages: es
+    .imageBucket()
+    .input(
+      z.object({
+        type: z.enum(["profile", "listing"]),
+      })
+    )
+    .path(({ input }) => [{ type: input.type }]),
 });
 
 const handler = createEdgeStoreNextHandler({
