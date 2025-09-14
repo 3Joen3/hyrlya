182046dcf002b0b010e0cfe47b68144d472edb8c
3Joen3 <joalevall@gmail.com>
2025-09-14 23:44:32 +0200

    Initial ProfileForm

---
+++ web/app/components/Forms/ProfileForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Form from "./Form";
+import PageHeading from "../PageHeading";
+import Block from "../Block";
+import SectionHeading from "../SectionHeading";
+import TextField from "./TextField";
+import ProfileFormImageSection from "./ProfileFormImageSection";
+import Button from "../Button";
+
+import { useForm } from "react-hook-form";
+import { ProfileData, profileSchema } from "@/lib/schemas/profileSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createLandlord } from "@/app/landlord/profile/create/actions";
+
+interface Props {
+  className: string;
+  heading: string;
+}
+
+export default function ProfileForm({ className, heading }: Props) {
+  const methods = useForm<ProfileData>({
+    resolver: zodResolver(profileSchema),
+  });
+
+  async function handleSubmit(data: ProfileData) {
+    await createLandlord(data);
+  }
+
+  return (
+    <Form
+      className={`${className} space-y-4`}
+      methods={methods}
+      onSubmit={handleSubmit}
+    >
+      <PageHeading heading={heading} />
+      <div className="grid grid-cols-3 gap-10">
+        <Block className="col-span-2 space-y-4">
+          <SectionHeading heading="Profil info" />
+          <TextField id="name" label="Namn" />
+          <TextField id="phoneNumber" label="Telefonnummer" />
+          <TextField id="emailAddress" label="Email address" />
+        </Block>
+
+        <Block className="col-span-1 flex flex-col justify-center items-center space-y-4">
+          <ProfileFormImageSection />
+        </Block>
+      </div>
+      <Button className="w-1/3" type="submit" color="secondary">
+        Spara
+      </Button>
+    </Form>
+  );
+}
---
+++ web/app/components/Forms/ProfileFormImageSection.tsx
@@ -0,0 +1,48 @@
+import ProfileImage from "../Profile/ProfileImage";
+import Button from "../Button";
+
+import { useEdgeStore } from "@/lib/edgestore";
+import { useRef, useState } from "react";
+import { useFormContext } from "react-hook-form";
+import SectionHeading from "../SectionHeading";
+
+export default function ProfileFormImageSection() {
+  const { edgestore } = useEdgeStore();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const [profileImageUrl, setProfileImageUrl] = useState<string>();
+
+  const { setValue } = useFormContext();
+
+  async function handleSelectedImage(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const response = await edgestore.publicImages.upload({
+      file,
+      input: { type: "profile" },
+    });
+
+    setProfileImageUrl(response.url);
+    setValue("profileImageUrl", response.url);
+  }
+
+  return (
+    <>
+      <SectionHeading heading="Profilbild" />
+      <input
+        className="hidden"
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={handleSelectedImage}
+      />
+
+      <ProfileImage className="w-1/2" imageUrl={profileImageUrl} />
+
+      <Button className="w-1/2" onClick={() => fileInputRef.current?.click()}>
+        Välj profilbild
+      </Button>
+    </>
+  );
+}
