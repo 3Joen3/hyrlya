e3b297ea657f51dcf8c932e2ac1bf6274f2b0884
3Joen3 <joalevall@gmail.com>
2025-09-14 17:46:58 +0200

    Create landlord request. Closes #7

---
+++ backend/Api/Requests/LandlordRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Requests
+{
+    public class LandlordRequest
+    {
+        [Required, MinLength(1)]
+        public string Name { get; init; } = default!;
+
+        [Required]
+        public LandlordContactRequest ContactMethod { get; init; } = default!;
+
+        public string? ProfileImageUrl { get; init; }
+    }
+
+    public class LandlordContactRequest : IValidatableObject
+    {
+        public string? PhoneNumber { get; init; }
+        public string? EmailAddress { get; init; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(PhoneNumber) && string.IsNullOrWhiteSpace(EmailAddress))
+                yield return new ValidationResult("A contact method is required. Phone number or email address.");
+        }
+    }
+}
