7f343cbc700763c75cf5644e65417fe0264af3b4
3Joen3 <joalevall@gmail.com>
2025-09-15 01:51:40 +0200

    CreateLandlord method

---
+++ web/app/landlord/profile/create/actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { postAuthenticated } from "@/lib/api/server";
+import { ProfileData, profileSchema } from "@/lib/schemas/profileSchema";
+import { redirect } from "next/navigation";
+
+export async function createLandlord(data: ProfileData) {
+  const parsed = profileSchema.parse(data);
+
+  const response = await postAuthenticated("landlords", parsed);
+
+  if (!response.ok) {
+    //Do Something
+  }
+
+  redirect("/landlord");
+}
