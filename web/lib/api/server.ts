3bca5ad68618fd5e5727075bfa427deea9046de2
3Joen3 <joalevall@gmail.com>
2025-09-14 18:49:57 +0200

    Methods to Post/Get with the accessToken

---
+++ web/lib/api/server.ts
@@ -0,0 +1,36 @@
+import "server-only";
+
+import { backendUrl } from "./utils";
+import { getAccessToken } from "../auth/cookies";
+
+export async function getAuthenticated(endpoint: string): Promise<Response> {
+  const url = `${backendUrl}/${endpoint}`;
+
+  const accessToken = await getAccessToken();
+
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return response;
+}
+
+export async function postAuthenticated(endpoint: string, data: any): Promise<Response> {
+  const url = `${backendUrl}/${endpoint}`;
+
+  const accessToken = await getAccessToken();
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return response;
+}
