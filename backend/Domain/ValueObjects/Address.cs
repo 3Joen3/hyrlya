d8d070dca342e19dc07068063f682ffd7765dddd
3Joen3 <joalevall@gmail.com>
2025-09-15 17:56:25 +0200

    Address ValueObject it's not really complete but it's enough for the use cases i think

---
+++ backend/Domain/ValueObjects/Address.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.ValueObjects
+{
+    public record Address
+    {
+        public string Street { get; }
+        public string HouseNumber { get; }
+        public string City { get; }
+
+        public Address(string street, string houseNumber, string city)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(street);
+            ArgumentException.ThrowIfNullOrWhiteSpace(houseNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(city);
+
+            Street = street;
+            HouseNumber = houseNumber;
+            City = city;
+        }
+    }
+}
---
+++ backend/Tests/Unit/ValueObjects/AddressTests.cs
@@ -0,0 +1,90 @@
+﻿using Domain.ValueObjects;
+
+namespace Tests.Unit.ValueObjects
+{
+    public class AddressTests
+    {
+        [Fact]
+        public void Constructor_WithFullValidParameters_ShouldSucceed()
+        {
+            var street = "SomeStreet";
+            var houseNumber = "123";
+            var city = "Borås";
+
+            var address = new Address(street, houseNumber, city);
+
+            Assert.Equal(street, address.Street);
+            Assert.Equal(houseNumber, address.HouseNumber);
+            Assert.Equal(city, address.City);
+        }
+
+        [Fact]
+        public void Constructor_WithNullStreet_ShouldThrow()
+        {
+            string? street = null!;
+            var houseNumber = "123";
+            var city = "Borås";
+
+            Assert.Throws<ArgumentNullException>(()
+                => new Address(street!, houseNumber, city));
+        }
+
+        [Fact]
+        public void Constructor_WithNullCity_ShouldThrow()
+        {
+            var street = "SomeStreet";
+            var houseNumber = "123";
+            string? city = null;
+
+            Assert.Throws<ArgumentNullException>(()
+                => new Address(street, houseNumber, city!));
+        }
+
+        [Fact]
+        public void Constructor_WithNullHouseNumber_ShouldThrow()
+        {
+            var street = "SomeStreet";
+            string? houseNumber = null;
+            var city = "Borås";
+
+            Assert.Throws<ArgumentNullException>(()
+                => new Address(street, houseNumber!, city!));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Constructor_WithInvalidStreet_ShouldThrow(string street)
+        {
+            var houseNumber = "123";
+            var city = "Borås";
+
+            Assert.Throws<ArgumentException>(()
+                => new Address(street, houseNumber, city));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Constructor_WithInvalidHouseNumber_ShouldThrow(string houseNumber)
+        {
+            var street = "SomeStreet";
+            var city = "Borås";
+
+            Assert.Throws<ArgumentException>(()
+                => new Address(street, houseNumber, city));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Constructor_WithInvalidCity_ShouldThrow(string city)
+        {
+            var street = "SomeStreet";
+            var houseNumber = "123";
+
+            Assert.Throws<ArgumentException>(()
+                => new Address(street, houseNumber, city));
+        }
+    }
+}
