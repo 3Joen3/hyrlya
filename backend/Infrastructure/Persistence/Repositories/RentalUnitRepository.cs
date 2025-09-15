1a27e5bc7c78879e2d4075c7f6e355864ff86e5f
3Joen3 <joalevall@gmail.com>
2025-09-15 21:40:30 +0200

    Add RentalUnit Repository

---
+++ backend/Infrastructure/Persistence/Repositories/RentalUnitRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class RentalUnitRepository(AppDbContext context) : IRentalUnitRepository
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<RentalUnit> AddAsync(RentalUnit rentalUnit)
+        {
+            _context.RentalUnits.Add(rentalUnit);
+            await _context.SaveChangesAsync();
+
+            return rentalUnit;
+        }
+    }
+}
