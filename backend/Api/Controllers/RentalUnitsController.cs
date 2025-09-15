94b15d658abd1bdc862ff299f788a810f88907a8
3Joen3 <joalevall@gmail.com>
2025-09-15 23:02:19 +0200

    Should now be possible to Create a RentalUnit

---
+++ backend/Api/Controllers/RentalUnitsController.cs
@@ -0,0 +1,29 @@
+﻿using Api.Requests;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RentalUnitsController(IRentalUnitService rentalUnitService) : ControllerBase
+    {
+        private readonly IRentalUnitService _rentalUnitService = rentalUnitService;
+
+        private string? IdentityId 
+            => User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMyRentalUnit([FromBody] RentalUnitRequest request)
+        {
+            if (string.IsNullOrEmpty(IdentityId))
+                return Unauthorized();
+
+            var rentalUnit = await _rentalUnitService.CreateAsync(IdentityId, request.Images,
+                request.Street, request.HouseNumber, request.City);
+
+            return Ok(rentalUnit);
+        }
+    }
+}
---
+++ backend/Api/Requests/RentalUnitRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Requests
+{
+    public class RentalUnitRequest : IValidatableObject
+    {
+        [Required]
+        public IEnumerable<string> Images { get; init; } = default!;
+
+        [Required, MinLength(1)]
+        public string Street { get; init; } = default!;
+
+        [Required, MinLength(1)]
+        public string HouseNumber { get; init; } = default!;
+
+        [Required, MinLength(1)]
+        public string City { get; init; } = default!;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!Images.Any())
+                yield return new ValidationResult("At least one image is required.");
+            else if (Images.Any(img => string.IsNullOrWhiteSpace(img)))
+                yield return new ValidationResult("An image can't be null or empty string.");
+        }
+    }
+}
