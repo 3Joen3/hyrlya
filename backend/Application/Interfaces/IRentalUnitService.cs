2f541cf5148060e2a021ee0d8ea0e464f534f170
3Joen3 <joalevall@gmail.com>
2025-09-15 22:13:46 +0200

    Initial CreateAsync for RentalUnit we need to look over later what to do when no Landlordid is found

---
+++ backend/Application/Interfaces/IRentalUnitService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IRentalUnitService
+    {
+        Task<RentalUnit> CreateAsync(string identityId, IEnumerable<string> imageUrls, 
+            string street, string houseNumber, string city);
+    }
+}
---
+++ backend/Application/Services/RentalUnitService.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using Domain.ValueObjects;
+
+namespace Application.Services
+{
+    public class RentalUnitService(IRentalUnitRepository repo, ILandlordService landlordService) : IRentalUnitService
+    {
+        private readonly IRentalUnitRepository _repo = repo;
+        private readonly ILandlordService _landlordService = landlordService;
+
+        public async Task<RentalUnit> CreateAsync(string identityId, IEnumerable<string> imageUrls, 
+            string street, string houseNumber, string city)
+        {
+            var landlordId = await _landlordService.GetIdByIdentityIdAsync(identityId);
+
+            if (landlordId == Guid.Empty)
+                return null;
+
+            var images = BuildRentalUnitImages(imageUrls);
+            var address = new Address(street, houseNumber, city);
+
+            var rentalUnit = new RentalUnit(images, address, landlordId);
+
+            return await _repo.AddAsync(rentalUnit);
+        }
+
+        private static IEnumerable<Image> BuildRentalUnitImages(IEnumerable<string> imageUrls)
+        {
+            var index = 1;
+            foreach (var imageUrl in imageUrls)
+            {
+                var url = new WebAddress(imageUrl);
+                var alt = $"Bostadsannons – bild {index}";
+
+                yield return new Image(url, alt);
+                index++;
+            }
+        }
+    }
+}
