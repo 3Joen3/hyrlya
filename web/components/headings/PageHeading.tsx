0ca797cd81c9e5c98d4a646522dec56f3aaf18fd
3Joen3 <joalevall@gmail.com>
2025-09-16 20:58:52 +0200

    Removed PageHeading

--- web/components/headings/PageHeading.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  className: string;
-  heading: string;
-}
-
-export default function PageHeading({ className, heading }: Props) {
-  return <h1 className={`${className} text-2xl font-bold underline`}>{heading}</h1>;
-}
