9a81491546699fabaf669e7682a739c31ca23aa4
3Joen3 <joalevall@gmail.com>
2025-09-18 00:15:24 +0200

    Since both Login and Register looks the same we will just use an AuthSchema instead of seperate ones

---
+++ web/lib/schemas/authSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const authSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+
+export type AuthData = z.infer<typeof authSchema>();
--- web/lib/schemas/registerSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const registerSchema = z.object({
-  email: z.email(),
-  password: z.string(),
-});
-
-export type RegisterData = z.infer<typeof registerSchema>;
