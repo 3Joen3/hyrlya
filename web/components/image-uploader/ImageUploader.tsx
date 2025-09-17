a3a5087f0573bc86a2fd4cfc515e3784dde65f5b
3Joen3 <joalevall@gmail.com>
2025-09-17 22:54:59 +0200

    Initial ImageUploader

---
+++ web/components/image-uploader/ImageUploader.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+import ImageUploaderDropZone from "./ImageUploaderDropZone";
+import ImageUploaderGallery from "./ImageUploaderGallery";
+
+import { useEdgeStore } from "@/lib/edgestore";
+import { useEffect, useState } from "react";
+
+interface Props {
+  id: string;
+}
+
+export default function ImageUploader({ id }: Props) {
+  const { edgestore } = useEdgeStore();
+  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
+  const { setValue } = useFormContext();
+
+  async function handleDroppedFiles(acceptedFiles: File[]) {
+    const responses = await Promise.all(
+      acceptedFiles.map((file) =>
+        edgestore.publicImages.upload({
+          file,
+          input: { type: "listing" },
+        })
+      )
+    );
+
+    const urls = responses.map((res) => res.url);
+    setUploadedImages((prev) => [...prev, ...urls]);
+  }
+
+  useEffect(() => {
+    setValue(id, uploadedImages);
+  }, [uploadedImages, id, setValue]);
+
+  return (
+    <div>
+      <ImageUploaderDropZone onDrop={handleDroppedFiles} />
+      <ImageUploaderGallery imageUrls={uploadedImages} />
+    </div>
+  );
+}
