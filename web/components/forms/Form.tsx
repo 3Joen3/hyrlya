393cd85890446cbc8f99467b6289a60e24f23042
3Joen3 <joalevall@gmail.com>
2025-09-18 05:35:02 +0200

    We might auctually be  able to have it in there

--- web/components/forms/Form.tsx
+++ web/components/forms/Form.tsx
@@ -1,9 +1,11 @@
+import PageHeading from "../PageHeading";
 import { FieldValues, FormProvider, UseFormReturn } from "react-hook-form";
 
 interface Props<T extends FieldValues> {
   className?: string;
   methods: UseFormReturn<T>;
   onSubmit: (data: T) => void;
+  heading: string;
   children: React.ReactNode;
 }
 
@@ -11,13 +13,15 @@ export default function Form<T extends FieldValues>({
   className,
   methods,
   onSubmit,
+  heading,
   children,
 }: Props<T>) {
   const { handleSubmit } = methods;
 
   return (
     <FormProvider {...methods}>
-      <form className={className} onSubmit={handleSubmit(onSubmit)}>
+      <form className={`${className} space-y-6`} onSubmit={handleSubmit(onSubmit)}>
+        <PageHeading heading={heading} />
         {children}
       </form>
     </FormProvider>
