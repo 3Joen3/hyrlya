2cc272f47a14243daa6ef7f384f149d9f939d9e0
3Joen3 <joalevall@gmail.com>
2025-09-19 17:00:52 +0200

    Added UnitOfWork

---
+++ backend/Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Interfaces;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task WriteChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
