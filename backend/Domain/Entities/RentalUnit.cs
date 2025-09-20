9f21d06b9916e9583a566c7a0a9ac457f8dd8608
3Joen3 <joalevall@gmail.com>
2025-09-21 01:52:26 +0200

    I removed the Guard class, i just dont like it

--- backend/Domain/Entities/RentalUnit.cs
+++ backend/Domain/Entities/RentalUnit.cs
@@ -19,13 +19,22 @@ namespace Domain.Entities
         public RentalUnit(Guid landlordId, Address address, RentalUnitType type, 
             int numberOfRooms, int sizeSquareMeters, IEnumerable<Image> images)
         {
-            Guard.AgainstEmpty(landlordId, nameof(Landlord));
-            Guard.AgainstNull(address, nameof(address));
-            Guard.AgainstOutOfRange(numberOfRooms, 1, nameof(numberOfRooms));
-            Guard.AgainstOutOfRange(sizeSquareMeters, 1, nameof(sizeSquareMeters));
-            Guard.AgainstNull(images, nameof(images));
-            Guard.AgainstEmptyCollection(images, nameof(images));
-             
+            if (landlordId == Guid.Empty)
+                throw new ArgumentException("LandlordId can't be empty.", nameof(landlordId));
+
+            ArgumentNullException.ThrowIfNull(address);
+
+            if (numberOfRooms < 1)
+                throw new ArgumentOutOfRangeException(nameof(numberOfRooms), "NumberOfRooms can't be under 1.");
+
+            if (sizeSquareMeters < 1)
+                throw new ArgumentOutOfRangeException(nameof(sizeSquareMeters), "SizeSquareMeters can't be under 1.");
+
+            ArgumentNullException.ThrowIfNull(images);
+
+            if (!images.Any())
+                throw new ArgumentException("At least one image is required.", nameof(images));
+
             LandlordId = landlordId;
             Address = address;
             Type = type;
@@ -36,7 +45,7 @@ namespace Domain.Entities
 
         public void ChangeAddress(Address newAddress)
         {
-            Guard.AgainstNull(newAddress, nameof(newAddress));
+            ArgumentNullException.ThrowIfNull(newAddress);
             Address = newAddress;
         }
 
@@ -44,34 +53,39 @@ namespace Domain.Entities
 
         public void ChangeNumberOfRooms(int newNumberOfRooms)
         {
-            Guard.AgainstOutOfRange(newNumberOfRooms, 1, nameof(newNumberOfRooms));
+            if (newNumberOfRooms < 1)
+                throw new ArgumentOutOfRangeException(nameof(newNumberOfRooms), "NumberOfRooms can't be under 1.");
+
             NumberOfRooms = newNumberOfRooms;
         }
 
         public void ChangeSizeSquareMeters(int newSize)
         {
-            Guard.AgainstOutOfRange(newSize, 1, nameof(newSize));
+            if (newSize < 1)
+                throw new ArgumentOutOfRangeException(nameof(newSize), "SizeSquareMeters can't be under 1.");
+
             SizeSquareMeters = newSize;
         }
 
         public void ReplaceImages(IEnumerable<Image> newImages)
         {
-            Guard.AgainstNull(newImages, nameof(newImages));
-            Guard.AgainstEmptyCollection(newImages, nameof(newImages));
+            ArgumentNullException.ThrowIfNull(newImages);
+
+            if (!newImages.Any())
+                throw new ArgumentException("At least one image is required.", nameof(newImages));
 
             _images.Clear();
             AddImages(newImages);
         }
 
         //NOT TEST
-        public bool IsOwnedBy(Guid landlordId) 
-            => LandlordId == landlordId;
+        public bool IsOwnedBy(Guid landlordId) => LandlordId == landlordId;
 
         private void AddImages(IEnumerable<Image> images)
         {
             foreach (var image in images)
             {
-                Guard.AgainstNull(image, nameof(image));
+                ArgumentNullException.ThrowIfNull(image);
                 _images.Add(image);
             }
         }
