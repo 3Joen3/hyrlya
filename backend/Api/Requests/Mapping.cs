41fabbedf4bdbea317a2845f605182f55a2a710e
3Joen3 <joalevall@gmail.com>
2025-09-20 10:28:17 +0200

    Added a static mapping class

---
+++ backend/Api/Requests/Mapping.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos;
+using Domain.ValueObjects;
+
+namespace Api.Requests
+{
+    public static class Mapping
+    {
+        public static RentalUnitDto ToDto(this RentalUnitRequest request)
+        {
+            var address = new Address(request.Address.Street, 
+                request.Address.HouseNumber, request.Address.City);
+
+            var imageUrls = request.ImageUrls
+                .Select(url => new WebAddress(url));
+
+            return new RentalUnitDto(address, request.Type, 
+                request.NumberOfRooms, request.SizeSquareMeters, imageUrls);
+        }
+    }
+}
