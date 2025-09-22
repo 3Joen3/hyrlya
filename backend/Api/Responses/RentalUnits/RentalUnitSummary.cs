501b2fae595bcbf12403fe719439627bfb636cf1
3Joen3 <joalevall@gmail.com>
2025-09-22 05:03:48 +0200

    Added type to the RentalUnitSummary

--- backend/Api/Responses/RentalUnits/RentalUnitSummary.cs
+++ backend/Api/Responses/RentalUnits/RentalUnitSummary.cs
@@ -6,6 +6,7 @@ namespace Api.Responses.RentalUnits
     public class RentalUnitSummary(RentalUnit rentalUnit)
     {
         public Guid Id { get; } = rentalUnit.Id;
+        public string Type { get; } = rentalUnit.Type.ToString();
         public Address Address { get; } = rentalUnit.Address;
     }
 }
