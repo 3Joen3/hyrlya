56db98f74c5e27dda8c6e6cbac1b82392baa957c
3Joen3 <joalevall@gmail.com>
2025-09-22 06:36:59 +0200

    added a put authenticated method

--- web/lib/api/server.ts
+++ web/lib/api/server.ts
@@ -9,6 +9,22 @@ export async function getAuthenticated(endpoint: string): Promise<Response> {
 
   const response = await fetch(url, {
     headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return response;
+}
+
+export async function postAuthenticated(endpoint: string, data: any): Promise<Response> {
+  const url = `${process.env.BACKEND_URL}/${endpoint}`;
+
+  const accessToken = await getAccessToken();
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${accessToken}`,
     },
@@ -17,16 +33,13 @@ export async function getAuthenticated(endpoint: string): Promise<Response> {
   return response;
 }
 
-export async function postAuthenticated(
-  endpoint: string,
-  data: any
-): Promise<Response> {
+export async function putAuthenticated(endpoint: string, data: any): Promise<Response> {
   const url = `${process.env.BACKEND_URL}/${endpoint}`;
 
   const accessToken = await getAccessToken();
 
   const response = await fetch(url, {
-    method: "POST",
+    method: "PUT",
     body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json",
