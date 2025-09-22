69e6a2cf66add54a59485cdf1b0a6d5331171fbe
3Joen3 <joalevall@gmail.com>
2025-09-22 06:47:09 +0200

    RentalUnit details response

--- backend/Api/Responses/RentalUnits/RentalUnitDetails.cs
+++ backend/Api/Responses/RentalUnits/RentalUnitDetails.cs
@@ -1,11 +1,16 @@
 ﻿using Api.Responses.Common;
 using Domain.Entities;
+using Domain.ValueObjects;
 
 namespace Api.Responses.RentalUnits
 {
     public class RentalUnitDetails(RentalUnit rentalUnit)
     {
-        public IEnumerable<ImageResponse> Images { get; } = rentalUnit.Images
-            .Select(img => new ImageResponse(img));
+        public Guid Id { get; } = rentalUnit.Id;
+        public string Type { get; } = rentalUnit.Type.ToString();
+        public Address Address { get; } = rentalUnit.Address;
+        public int NumberOfRooms { get; } = rentalUnit.NumberOfRooms;
+        public int SizeSquareMeters { get; } = rentalUnit.SizeSquareMeters;
+        public IEnumerable<ImageResponse> Images { get; } = rentalUnit.Images.Select(img => new ImageResponse(img));
     }
 }
