468feceb250195ba67a4012641685e1524bcafbe
3Joen3 <joalevall@gmail.com>
2025-09-23 05:10:19 +0200

    Remove flex 1 from page

--- web/components/Page.tsx
+++ web/components/Page.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Page({ heading, className, children }: Props) {
   return (
-    <main className={`w-11/12 mx-auto flex-1 ${heading ? "space-y-6" : ""}`}>
+    <main className={`w-11/12 mx-auto ${heading ? "space-y-6" : ""}`}>
       {heading && <h1 className="text-2xl font-bold underline">{heading}</h1>}
       <div className={className}>{children}</div>
     </main>
