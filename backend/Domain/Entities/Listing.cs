b4bc194a2922d0fe5c86128137acdf05d971a1f6
3Joen3 <joalevall@gmail.com>
2025-09-24 11:53:14 +0200

    Added RentalUnit and RentalType to listing

--- backend/Domain/Entities/Listing.cs
+++ backend/Domain/Entities/Listing.cs
@@ -1,17 +1,25 @@
-﻿using Domain.ValueObjects;
+﻿using Domain.Enums;
 
 namespace Domain.Entities
 {
     public class Listing : Entity
     {
-        public RentalPeriod RentalPeriod { get; private set; }
-        
         public Guid LandlordId { get; private set; }
         public Landlord? Landlord { get; private set; }
 
-        public Listing(Guid landlordId)
+        public RentalUnit RentalUnit { get; private set; }
+        public RentalType RentalType { get; set; }
+
+        public Listing(Guid landlordId, RentalUnit rentalUnit, RentalType rentalType) 
         {
             LandlordId = SetLandlordId(landlordId);
+            RentalUnit = SetRentalUnit(rentalUnit);
+            RentalType = rentalType;
+        }
+
+        private Listing() 
+        {
+            RentalUnit = default!;
         }
 
         private static Guid SetLandlordId(Guid landlordId)
@@ -20,6 +28,12 @@ namespace Domain.Entities
                 throw new ArgumentException("LandlordId can't be empty.", nameof(landlordId));
 
             return landlordId;
+        }
+
+        private static RentalUnit SetRentalUnit(RentalUnit rentalUnit)
+        {
+            ArgumentNullException.ThrowIfNull(rentalUnit);
+            return rentalUnit;
         }
     }
 }
