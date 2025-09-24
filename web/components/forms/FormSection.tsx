ca7c1a885f8cfd7ad6a4314a931fa1603bafa6a3
3Joen3 <joalevall@gmail.com>
2025-09-24 15:03:29 +0200

    FormSection

---
+++ web/components/forms/FormSection.tsx
@@ -0,0 +1,23 @@
+import SectionHeading from "../SectionHeading";
+
+interface Props {
+  heading?: string;
+  children: React.ReactNode;
+}
+
+export default function FormSection({ heading, children }: Props) {
+  if (heading) {
+    return <WithHeading heading={heading}>{children}</WithHeading>;
+  }
+
+  return <div className="space-y-4">{children}</div>;
+}
+
+function WithHeading({ heading, children }: { heading: string; children: React.ReactNode }) {
+  return (
+    <div className="space-y-6">
+      <SectionHeading heading={heading} />
+      <div className="space-y-4">{children}</div>
+    </div>
+  );
+}
