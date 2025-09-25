1081bf3642052c545aa740ceb95fbce5886cc265
3Joen3 <joalevall@gmail.com>
2025-09-25 08:32:15 +0200

    Add MB to PageTopRow

--- web/components/PageTopRow.tsx
+++ web/components/PageTopRow.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function PageTopRow({ heading, children }: Props) {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center mb-6">
       <h1 className="text-2xl font-bold underline">{heading}</h1>
       <div>{children}</div>
     </div>
