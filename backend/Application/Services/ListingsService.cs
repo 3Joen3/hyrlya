da775d50dde2f914eb950b674cd2eaf3981e8544
3Joen3 <joalevall@gmail.com>
2025-09-26 21:11:22 +0200

    GetPaginated listings Service

---
+++ backend/Application/Services/ListingsService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class ListingsService(IListingRepository repo) : IListingService
+    {
+        private readonly IListingRepository _repo = repo;
+
+        public async Task<IEnumerable<Listing>> GetPaginatedAsync(int page, int pageSize)
+        {
+            var listings = await _repo.GetPaginatedAsync(page, pageSize);
+            return listings;
+        }
+    }
+}
