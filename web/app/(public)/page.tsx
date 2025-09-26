bb9c4040afec77eb4e81a1280b585cf1d50a22ed
3Joen3 <joalevall@gmail.com>
2025-09-26 08:12:05 +0200

    Fixed an error

--- web/app/(public)/page.tsx
+++ web/app/(public)/page.tsx
@@ -2,8 +2,8 @@ import Page from "@/components/Page";
 
 export default function Home() {
   return (
-    <Page heading="Bostadsannonser" className="">
-      <h1>FrontPage</h1>
+    <Page>
+      <p>Bostadsannonser</p>
     </Page>
   );
 }
