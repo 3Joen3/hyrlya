dd47b50a4e9d090aab060628f177a7c51b1ec012
3Joen3 <joalevall@gmail.com>
2025-09-26 21:15:12 +0200

    ListingsController

---
+++ backend/Api/Controllers/ListingsController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListingsController(IListingService listingService) : ControllerBase
+    {
+        private readonly IListingService _listingsService = listingService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetPaginated([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            var result = await _listingsService.GetPaginatedAsync(page, pageSize);
+            return Ok(result);
+        }
+    }
+}
+    
