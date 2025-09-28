bc28a94034eda1922019e587f5c2518796edd8aa
3Joen3 <joalevall@gmail.com>
2025-09-28 12:57:25 +0200

    Add ErrorMessage to textfield

--- web/components/forms/TextField.tsx
+++ web/components/forms/TextField.tsx
@@ -1,5 +1,3 @@
-import FormField from "./FormField";
-
 import { useFormContext } from "react-hook-form";
 
 interface Props {
@@ -9,16 +7,29 @@ interface Props {
 }
 
 export default function TextField({ type = "text", id, label }: Props) {
-  const { register } = useFormContext();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const error = errors[id]?.message as string;
 
   return (
-    <FormField id={id} label={label}>
+    <div className="flex flex-col space-y-1">
+      <label htmlFor={id} className="text-lg">
+        {label}
+      </label>
       <input
-        className="rounded border border-neutral-300 px-2 py-1.5 focus:outline-none focus:border-sky-600 focus:ring-1 focus:ring-sky-600"
+        className={`rounded border px-2 py-1.5 focus:outline-none focus:ring-1 ${
+          error
+            ? "border-red-500 focus:border-red-500 focus:ring-red-500"
+            : "border-neutral-300   focus:border-sky-600  focus:ring-sky-600"
+        }`}
         type={type}
         id={id}
         {...register(id)}
       />
-    </FormField>
+      {error && <p className="text-red-600">{error}</p>}
+    </div>
   );
 }
