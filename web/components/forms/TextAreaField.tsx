cd902c0c8d75a578a175225083dedfccab5549fe
3Joen3 <joalevall@gmail.com>
2025-09-28 17:35:48 +0200

    TextAreaField

---
+++ web/components/forms/TextAreaField.tsx
@@ -0,0 +1,32 @@
+import { useFormContext } from "react-hook-form";
+
+interface Props {
+  id: string;
+  label: string;
+}
+
+export default function TextAreaField({ id, label }: Props) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const error = errors[id]?.message as string;
+
+  return (
+    <div className="flex flex-col space-y-1">
+      <label className="text-lg" htmlFor={id}>
+        {label}
+      </label>
+      <textarea
+        {...register(id)}
+        className={`rounded border px-2 py-1.5 focus:outline-none focus:ring-1 ${
+          error
+            ? "border-red-500 focus:border-red-500 focus:ring-red-500"
+            : "border-neutral-300   focus:border-sky-600  focus:ring-sky-600"
+        }`}
+      />
+      {error && <p className="text-red-600">{error}</p>}
+    </div>
+  );
+} 
