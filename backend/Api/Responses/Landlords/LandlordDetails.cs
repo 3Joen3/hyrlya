dbee81ad33dd1a95092e8a237d2a414fd7748995
3Joen3 <joalevall@gmail.com>
2025-09-28 11:13:40 +0200

    Added Phone and Email to Landlord Details

--- backend/Api/Responses/Landlords/LandlordDetails.cs
+++ backend/Api/Responses/Landlords/LandlordDetails.cs
@@ -1,16 +1,9 @@
-﻿using Api.Responses.Common;
-using Domain.Entities;
+﻿using Domain.Entities;
 
 namespace Api.Responses.Landlords
 {
     public class LandlordDetails(Landlord landlord)
     {
         public LandlordProfileDetails Profile { get; } = new LandlordProfileDetails(landlord.Profile);
-    }
-
-    public class LandlordProfileDetails(LandlordProfile profile)
-    {
-        public string Name { get; } = profile.Name;
-        public ImageResponse Image { get; } = new ImageResponse(profile.Image);
     }
 }
---
+++ backend/Api/Responses/Landlords/LandlordProfileDetails.cs
@@ -0,0 +1,13 @@
+﻿using Api.Responses.Common;
+using Domain.Entities;
+
+namespace Api.Responses.Landlords
+{
+    public class LandlordProfileDetails(LandlordProfile profile)
+    {
+        public string Name { get; } = profile.Name;
+        public ImageResponse? Image { get; } = profile.Image is not null ? new ImageResponse(profile.Image) : null;
+        public string? Phone { get; } = profile.PhoneNumber?.Value;
+        public string? Email { get; } = profile.EmailAddress?.Value;
+    }
+}
