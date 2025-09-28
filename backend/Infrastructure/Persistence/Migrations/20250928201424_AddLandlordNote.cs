5855b0f34a9e9ca18295a87acba9a831291a9045
3Joen3 <joalevall@gmail.com>
2025-09-28 22:14:32 +0200

    Migrations

---
+++ backend/Infrastructure/Persistence/Migrations/20250928201424_AddLandlordNote.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLandlordNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LandlordNote",
+                table: "Listings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LandlordNote",
+                table: "Listings");
+        }
+    }
+}
