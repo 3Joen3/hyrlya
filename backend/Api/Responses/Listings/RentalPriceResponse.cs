000fe86b5122b34f07e7095e8827f4fcf49b8675
3Joen3 <joalevall@gmail.com>
2025-09-28 11:14:24 +0200

    RentalPrice Response so that we can return a string. This way it's easier for the frontend.

---
+++ backend/Api/Responses/Listings/RentalPriceResponse.cs
@@ -0,0 +1,10 @@
+﻿using Domain.ValueObjects;
+
+namespace Api.Responses.Listings
+{
+    public class RentalPriceResponse(RentalPrice rentalPrice)
+    {
+        public decimal Amount { get; } = rentalPrice.Amount;
+        public string ChargeInterval { get; } = rentalPrice.ChargeInterval.ToString();
+    }
+}
