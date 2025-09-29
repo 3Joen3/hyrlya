731a1630a9e804f144b51908593bbb6efe9f4528
3Joen3 <joalevall@gmail.com>
2025-09-29 19:23:48 +0200

    Login redirects to /landlord/listings instead

--- web/app/(public)/auth/page.tsx
+++ web/app/(public)/auth/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 
 export default async function page() {
   if (await getAccessToken()) {
-    redirect("/landlord");
+    redirect("/landlord/listings");
   }
 
   return (
