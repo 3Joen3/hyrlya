cb6fcc15c481e21e3d0496d31f395bd166d13a09
3Joen3 <joalevall@gmail.com>
2025-09-30 16:14:07 +0200

    Changed the profile image component

--- web/ui/ListingPage.tsx/LandlordContact.tsx
+++ web/ui/ListingPage.tsx/LandlordContact.tsx
@@ -1,5 +1,5 @@
 import ListingPageIconRow from "./ListingPageIconRow";
-import Image from "next/image";
+import ProfileImage from "../ProfileImage";
 
 import { Image as ImageType } from "@/types/Common";
 import { EnvelopeIcon, PhoneIcon, UserIcon } from "@heroicons/react/24/outline";
@@ -14,7 +14,7 @@ interface Props {
 export default function LandlordContact({ name, phone, email, image }: Props) {
   return (
     <div className="flex flex-col md:flex-row gap-6">
-      <Image src={image.url} alt={image.altText} width={120} height={120} />
+      <ProfileImage image={image} />
       <div className="space-y-3">
         <h2 className="text-xl font-semibold">Kontakta hyresvärden!</h2>
         <Icons name={name} phone={phone} email={email} />
--- web/ui/ProfileImage.tsx
+++ web/ui/ProfileImage.tsx
@@ -1,30 +1,13 @@
 import Image from "next/image";
+
+import { Image as ImageType } from "@/types/Common";
 import { UserIcon } from "@heroicons/react/24/outline";
 
 interface Props {
-  className: string;
-  imageUrl?: string;
+  image?: ImageType;
 }
 
-export default function ProfileImage({ className, imageUrl }: Props) {
-  return (
-    <div
-      className={`${className} aspect-square rounded-full overflow-hidden bg-neutral-300 flex items-center justify-center`}
-    >
-      {imageUrl ? <UserImage imageUrl={imageUrl} /> : <Placeholder />}
-    </div>
-  );
-}
-
-function UserImage({ imageUrl }: { imageUrl: string }) {
-  return (
-    <div className="relative w-full h-full">
-      <Image className="object-cover" src={imageUrl} alt="Something" fill />
-      <p></p>
-    </div>
-  );
-}
-
-function Placeholder() {
-  return <UserIcon className="w-1/2" />;
+export default function ProfileImage({ image }: Props) {
+  if (image) return <Image src={image.url} alt={image.altText} width={120} height={120} />;
+  return <UserIcon className="w-[120px] h-[120px]" />;
 }
