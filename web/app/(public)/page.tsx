76f9a9db952e8bd32e08a077a6832939ef3e20f9
3Joen3 <joalevall@gmail.com>
2025-09-30 12:54:45 +0200

    Responsive public listing page

--- web/app/(public)/page.tsx
+++ web/app/(public)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     <Page>
       <PageTopRow heading="Bostadsannonser" />
 
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {listings.map((listing) => (
           <ListingCard key={listing.id} {...listing} image={listing.image} />
         ))}
