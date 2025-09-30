882fc8015b7631a9821bfb4cdc8be6c0b5c81417
3Joen3 <joalevall@gmail.com>
2025-09-30 14:37:23 +0200

    Remove outer classname will just set it IN the form instead

--- web/app/landlord/page.tsx
+++ web/app/landlord/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
   return (
     <Page>
       <PageTopRow heading="Kom igång som hyresvärd" />
-      <ProfileForm className="w-full md:w-3/4 lg:w-2/3" />
+      <ProfileForm />
     </Page>
   );
 }
