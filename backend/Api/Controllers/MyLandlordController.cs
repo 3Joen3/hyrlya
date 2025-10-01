ff2fe0c5581733940d40dbcee49fd850936ee6ee
3Joen3 <joalevall@gmail.com>
2025-10-02 01:57:58 +0200

    TryCatch in LandlordController

--- backend/Api/Controllers/MyLandlordController.cs
+++ backend/Api/Controllers/MyLandlordController.cs
@@ -20,32 +20,53 @@ namespace Api.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMyLandlord([FromBody] LandlordRequest request)
         {
-            var dto = request.ToDto();
-            var landlord = await _myLandlordService.CreateAsync(IdentityId, dto);
+            try
+            {
+                var dto = request.ToDto();
+                var landlord = await _myLandlordService.CreateAsync(IdentityId, dto);
 
-            return Ok(landlord);
+                return Ok(landlord);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict();
+            }
         }
 
         [HttpPut]
         public async Task<IActionResult> UpdateMyLandlordProfile([FromBody] LandlordRequest request)
         {
-            var dto = request.ToDto();
-            var profile = await _myLandlordService.UpdateProfileAsync(IdentityId, dto);
-            return Ok(profile);
+            try
+            {
+                var dto = request.ToDto();
+                var profile = await _myLandlordService.UpdateProfileAsync(IdentityId, dto);
+                return Ok(profile);
+            }
+            catch(KeyNotFoundException)
+            {
+                return NotFound();
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetMyLandlordProfile()
         {
-            var profile = await _myLandlordService
-                .GetProfileAsync(IdentityId);
+            try
+            {
+                var profile = await _myLandlordService
+                    .GetProfileAsync(IdentityId);
 
-            if (profile is null)
+                if (profile is null)
+                    return NotFound();
+
+                var response = new LandlordProfileDetails(profile);
+
+                return Ok(response);
+            }
+            catch (KeyNotFoundException)
+            {
                 return NotFound();
-
-            var response = new LandlordProfileDetails(profile);
-
-            return Ok(response);
+            }
         }
     }
 }
